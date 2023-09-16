5674a72704949ac1ccbb36e779491933d51d2a71
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2023-09-16 19:18:59 +0200

    amzn

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -9,17 +9,18 @@ const Intro = () => {
           <div className="intro__texts">
             <h1 className="intro__name intro__rainbow">Tommaso Romanò</h1>
             <p className="intro__job intro__job-main">
-              Software Engineer @ Amazon
+              Software Engineer @ Amazon Robotics
             </p>
 
             <p className="intro__job mt-2">
-              Hello! I&apos;m a Software Engineer and Data Scientist from Milan, Italy;
-              MSc Computer Science student <span className='minor-links'>@ UNIMI</span>.
+              Hello! I&apos;m a Software Engineer from Milan, Italy 🇮🇹;
+              Currently in Berlin, Germany 🇩🇪, working as a Software Engineer <span className='minor-links'>@ Amazon Robotics</span> and
+              MSc Computer Science student <span className='minor-links'>@ University of Milan</span>.
             </p>
 
             <p className="intro__job mt-2">
-            You&apos;ll find writing about technologies. I&apos;m interested 
-            in at the time, or how I&apos;m learning and growing in my career, sharing knowledge along the way.
+            I&apos;m passionate about building software that solves real-world problems.
+            Here, you&apos;ll find how I&apos;m learning and growing in my career, sharing it along the way.
             </p>
           </div>
           <div className="intro__image">

55444de39e4f1c47228536ce11b6a11ae4155bae
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2024-04-18 14:32:44 +0200

    init

--- pages/recognitions.tsx
+++ pages/recognitions.tsx
@@ -1,74 +1,29 @@
-import { NextPage } from 'next';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import MetaHead from '../components/MetaHead';
-import Navigation from '../components/Navigation';
-import Project from '../components/Project';
-import _data from '../data/recognitions.json';
-import { useState } from 'react';
-import Section from '../components/Section';
-import Recognition from '../components/Recognition';
+import { NextPage } from "next";
+import MetaHead from "../components/MetaHead";
+import Footer from "../components/Footer";
+import { allRecognitions } from "../components/Recognition";
 
 const Recognitions: NextPage = () => {
-
-  const data = _data.filter((p) => p.show === true)
-  const filters = ["All","Selected"]
-  const [curFilter,setFilter] = useState(0);
-
-  const filtered = (filter:number) => {
-    if (filter === 0) {return data;}
-    if (filter === 1) {return data.filter((p)=>p.featured === true);}
-    return data.filter((p,i) => {
-      return p.show === true && p.tags.includes(filters[filter]);
-    })
-  }
-
-  const renderFilters = () => {
-    return (
-      <p>{filters.map((f,i) =>
-        i == curFilter ?
-        <>
-        <span className="text-black-dark hover:underline font-black"
-        onClick={()=>{setFilter(i)}}> {f} ({filtered(i).length}) </span>
-        {i+1 !== filters.length && "/"}
-        </>
-        : 
-        <>
-        <span className="hover:underline"
-        onClick={()=>{setFilter(i)}}> {f} ({filtered(i).length}) </span>
-        {i+1 !== filters.length && "/"}
-        </>
-      )}</p>
-    )
-  }
-
   return (
-  <div>
-    <MetaHead
-      title="Recognitions | Tommaso Romanò"
-      ogtitle="Recognitions made by Tommaso Romanò"
-      description="A selected list of recognitions I've received over the years including news, articles, interviews, awards, expositions, events, talks, courses and achievements."
-      url="https://tommasoromano.com/recognitions"
-    />
-    <Navigation />
-    <div className="py-8"></div>
+    <div className="bg-gray-900 text-white">
+      <MetaHead
+        title="Recognitions | Tommaso Romanò"
+        ogtitle="Recognitions made by Tommaso Romanò"
+        description="A selected list of recognitions I've received over the years including news, articles, interviews, awards, expositions, events, talks, courses and achievements."
+        url="https://tommasoromano.com/recognitions"
+      />
+      <section className="mx-auto max-w-screen-sm px-4">
+        <main className="mb-auto">
+          <div className="pt-8 mb-4 text-2xl font-bold">Recognitions</div>
+          <div className="flex w-full flex-col space-y-8">
+            {allRecognitions.map((e) => e)}
+          </div>
+        </main>
 
-    <Section
-      title="Recognitions"
-      description="A selected list of recognitions I've received over the years including news, articles, interviews, awards, expositions, events, talks, courses and achievements."
-      subtitle={renderFilters()}
-    >
-      {filtered(curFilter).map((recognition, index) => (
-            <Recognition key={index}
-            recognition={recognition}
-            />
-      ))}
-    </Section>
-
-    <Contact />
-    <Footer />
-  </div>
-);
-}
+        <Footer />
+      </section>
+    </div>
+  );
+};
 
 export default Recognitions;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,103 @@
-/** @type {import('tailwindcss').Config} */
+// @ts-check
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import("tailwindcss/types").Config } */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './posts/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/pliny/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+    "./layouts/**/*.{js,ts,tsx}",
+    "./data/**/*.mdx",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        primary: 'var(--c-primary)',
-        'light-gray': 'var(--c-gray-light)',
-        'med-gray': 'var(--c-text-gray)',
-        'black-dark': 'var(--c-black-dark)'
+      animation: {
+        hey: "hey 2.4s infinite",
       },
+      keyframes: {
+        hey: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "10%": {
+            transform: "rotate(16deg)",
+          },
+          "20%": {
+            transform: "rotate(-10deg)",
+          },
+          "30%": {
+            transform: "rotate(16deg)",
+          },
+          "40%": {
+            transform: "rotate(-10deg)",
+          },
+          "50%": {
+            transform: "rotate(12deg)",
+          },
+          "60%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
+          },
+        },
+      },
+      lineHeight: {
+        11: "2.75rem",
+        12: "3rem",
+        13: "3.25rem",
+        14: "3.5rem",
+      },
+      fontFamily: {
+        sans: ["var(--font-space-grotesk)", ...fontFamily.sans],
+      },
+      colors: {
+        primary: colors.sky,
+        secondary: colors.pink,
+        gray: colors.gray,
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.600")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2": {
+              fontWeight: "700",
+              letterSpacing: theme("letterSpacing.tight"),
+            },
+            h3: {
+              fontWeight: "600",
+            },
+            code: {
+              color: theme("colors.indigo.500"),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.400")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2,h3,h4,h5,h6": {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };

ea029b897c84821ac1ab0cc81e832452642ef8d7
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2024-04-22 10:55:02 +0200

    resume

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,6 +26,11 @@ export default function Footer() {
           />
           {/* <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
+          <SocialIcon
+            kind="resume"
+            href={"https://tommasoromano.com/tommaso_romano_resume.pdf"}
+            size={6}
+          />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           {/* <div>{siteMetadata.author}</div>
